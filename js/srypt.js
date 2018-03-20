afe5d88972d2662a15f7c4b48b6cd526691f1a4d
kid1313 <34199130+kid1313@users.noreply.github.com>
2018-03-20 20:14:44 +0300

    1

---
+++ js/srypt.js
@@ -0,0 +1,14 @@
+﻿window.onload=function(){
+  document.querySelector('.header__menu_help').onmouseover = function(){
+   	document.querySelector('.popup_menu').style.display = "block";
+  };
+  document.querySelector('.popup_menu').onmouseover =function(){
+    document.querySelector('.popup_menu').style.display = "block";
+  };
+  document.querySelector('.popup_menu').onmouseout =function(){
+    document.querySelector('.popup_menu').style.display = "";
+  };
+  document.querySelector('.header__menu_help').onmouseout = function(){
+    document.querySelector('.popup_menu').style.display = "";
+  };
+};
