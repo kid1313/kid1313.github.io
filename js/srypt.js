846d7a3765cc47cfe3cb22c1ea27b4b32fe8b7c3
kid1313 <34199130+kid1313@users.noreply.github.com>
2018-03-30 14:38:43 +0300

    1

--- js/srypt.js
+++ js/srypt.js
@@ -11,4 +11,16 @@
   document.querySelector('.header__menu_help').onmouseout = function(){
     document.querySelector('.popup_menu').style.display = "";
   };
+  ScrollUp();
+};
+
+function capab_click(id){
+  location.href="pages/capabilities.html" + "#" + id;
+  return false;
+};
+
+function ScrollUp(){
+  if(window.location.href.indexOf("#") > 0){
+    window.scrollBy(0, -120);
+  }
 };
