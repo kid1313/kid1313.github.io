06039f33f6eb27631b8f85e94bf693ab26d35eb3
kid1313 <34199130+kid1313@users.noreply.github.com>
2018-04-01 01:00:52 +0300

    1

--- js/srypt.js
+++ js/srypt.js
@@ -27,15 +27,8 @@
       location.href="../index.html";
   });
   
-  //---------Видеообзор на главной------------------------------------------------
-  document.querySelector('.main__theme__video').addEventListener("click", function(event){
-    event.preventDefault();
-    document.querySelector('.modal_cover').style.display = "block";
-    document.querySelector('.modal_video').style.display = "block";
-    document.getElementsByClassName('review')[0].src = "https://www.youtube.com/embed/z5xDj658esg?rel=0&autoplay=1";
-  });
 
-//---------Закрытие видео--------------------------------------------------------
+//---------Отслеживание события закрытия для закрытия видео-------------------------------------------
 document.querySelector('.modal_cover').addEventListener("click", function(event){
     event.preventDefault();
     CloseModal();
@@ -56,10 +49,11 @@ document.onkeydown = function(event) {
 
 };
 
+//-------------Закрытие модального окна видео--------------------------------------------------
 function CloseModal(){
   document.querySelector('.modal_cover').style.display = "none";
   document.querySelector('.modal_video').style.display = "none";
-  document.getElementsByClassName('review')[0].src = "not.found/404";
+  document.getElementsByClassName('review')[0].src = "";
 }
 
 function capab_click(id){ // переход на страницу возможностей, на соответсвующий пункт
@@ -68,8 +62,92 @@ function capab_click(id){ // переход на страницу возможн
 };
 
 function ScrollUp(){ //скролит вверх, чтобы курсор позиционировался на нужном пункте. Сдвиг 120px из-за фиксированного меню 
-  if(window.location.href.indexOf("#") > 0){
+  if(window.location.href.indexOf("#d") > 0){
     window.scrollBy(0, -120);
   }
 };
 
+//------------------------Видео в модальном окне---------------------------------------------
+function onPlay(event, id){
+  event.preventDefault();
+  document.querySelector('.modal_cover').style.display = "block";
+  document.querySelector('.modal_video').style.display = "block";
+  if(id == "main_review"){
+    document.getElementsByClassName('review')[0].src = "https://www.youtube.com/embed/z5xDj658esg?rel=0&autoplay=1";
+  }
+  if(id == "setup_review"){
+    document.getElementsByClassName('review')[0].src = "https://www.youtube.com/embed/C4e974kerTc?rel=0&autoplay=1";
+  }
+  return false;
+};
+
+
+//-------------------------------Обраюотки страницы "Справка"------------------------------
+
+//--------------------------------Разворачивание меню-------------------------------------
+
+function  onClickMenu(id){ 
+  var menuSubListArr = document.getElementsByClassName('help_menu__sub_list'); // список подменю
+  var menuListArr = document.getElementsByClassName('help_menu__item'); //основное 
+  for(var i = 0; i < menuSubListArr.length; i++)
+  {
+    if(menuSubListArr[i].id != id + "_sub")
+    {
+      if(!menuSubListArr[i].classList.contains('help_hide'))
+        menuSubListArr[i].classList.add('help_hide');
+    }
+    else
+      menuSubListArr[i].classList.remove('help_hide');
+  }
+  for(var j = 0; j < menuListArr.length; j++ )
+  {
+    if(menuListArr[j].id == id)
+      menuListArr[j].classList.add('active_mark');
+    else
+      menuListArr[j].classList.remove('active_mark');
+  }
+  onClickSubMenu(document.querySelectorAll("#" + id + "_sub" + " > li:first-child")[0].id); // первый элемент в выбраном меню 
+  return false;
+}
+
+//-------------------------------Открытие статьи----------------------------------------
+
+function onClickSubMenu(id){
+  var article_id = articleId(id).join(''); // массив->строка
+  var menuSubItemArr = document.getElementsByClassName('help_menu__sub_item'); //элементы списка подменю
+  var articleItemArr = document.getElementsByClassName('help_article__item'); // массив статей
+  document.getElementById(id).classList.add('active_mark');
+  for(var i = 0; i < menuSubItemArr.length; i++)
+  {
+    if(menuSubItemArr[i].id != id && menuSubItemArr[i].classList.contains('active_mark'))
+      menuSubItemArr[i].classList.remove('active_mark');
+  }
+  for(var i = 0; i < articleItemArr.length; i++)
+  {
+    if(articleItemArr[i].id != article_id)
+    {
+      if(!articleItemArr[i].classList.contains('help_hide'))
+        articleItemArr[i].classList.add('help_hide');
+    }
+    else
+    {
+      articleItemArr[i].classList.remove('help_hide');
+    }
+  }
+}
+
+
+//------------------------------Определение какую статью открыть-----------------------
+
+// структура пути <основное меню>_<подменю>-<статья>
+function articleId(id){
+  var article_id = [];
+  var j = 0;
+  for(var i = id.indexOf("-") + 1; i < id.length; i++) //записываем в переменную все что после "-"
+  {
+    article_id[j] = id[i];
+    j++;
+  }
+  return article_id;  
+
+}
